446bdc48ecf4f8e13d551a01dbc9c45a3c90c338
qding109 <qding109@gmail.com>
2021-02-20 16:41:24 +0800

    更改了master分支下的一个代码 Please enter the commit message for your changes. Lines starting

--- src/main/java/indi/qding/java/字符串的处理/StringJoiner用于字符串拼接.java
+++ src/main/java/indi/qding/java/字符串的处理/StringJoiner用于字符串拼接.java
@@ -13,7 +13,7 @@ public class StringJoiner用于字符串拼接 {
     public static void main(String[] args) {
         //用于sql拼接会非常的简单
         StringJoiner stringJoiner = new StringJoiner("','", "('", "')");
-        stringJoiner.add("8613177865659");
+        stringJoiner.add("8615180450284");
         stringJoiner.add("8615180450284");
         stringJoiner.add("8615180450284");
 
