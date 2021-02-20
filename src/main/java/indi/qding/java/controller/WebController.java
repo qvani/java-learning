03ce51da435367c2aeb69b1a8bf1362879a14d14
qding109 <qding109@gmail.com>
2021-02-20 17:08:00 +0800

    change

--- src/main/java/indi/qding/java/controller/WebController.java
+++ src/main/java/indi/qding/java/controller/WebController.java
@@ -10,4 +10,9 @@ public class WebController {
     public String toSuccess(){
         return "success";
     }
+
+    @GetMapping("/failed")
+    public String failed(){
+        return "not good!";
+    }
 }
